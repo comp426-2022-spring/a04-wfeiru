26a03b0a5bf02dd2eeeef1eb0c19fcf68271d578
Ruth Wu <rwu02@email.unc.edu>
2022-04-09 02:29:27 -0400

    documentation

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 import { createRequire } from 'module';
 import { coinFlip, coinFlips, countFlips, flipACoin } from './modules/coin.mjs';
 
-// require Express.js
+// require express module
 const require = createRequire(import.meta.url);
 const express = require('express');
 const app = express();
 
-// start an app server
+// require minimist module
 const args = require('minimist')(process.argv.slice(2));
-args['port'];
+
+// see what is stored in the object produced by minimist
+// console.log(args)
+
+// store help text 
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+
+// if --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
+// start an app server
 const port = args.port||process.env.PORT||3000;
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port));
